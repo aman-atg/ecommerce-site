78205f441322c09d7b6522f1563cbb80d9b3d096
aman-atg <amanansari.a2@gmail.com>
2020-04-24 22:03:43 +0530

    using cartIcon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,6 +8,7 @@ import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
+import CartIcon from "../cart-icon/cart-icon.component";
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -35,6 +36,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
