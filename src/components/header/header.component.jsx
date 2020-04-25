3e0b0b5ce602b696128f14e0edb5d9e42e627bc8
aman-atg <amanansari.a2@gmail.com>
2020-04-25 18:01:15 +0530

    update

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,8 +9,9 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
 import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -37,12 +38,15 @@ const Header = ({ currentUser }) => (
         </Link>
       )}
       <CartIcon />
+
+      {hidden ? null : <CartDropdown />}
     </div>
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
