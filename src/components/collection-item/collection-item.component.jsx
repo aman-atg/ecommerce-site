a5877139fa3c0497901492a6e421dbd24b437019
aman-atg <amanansari.a2@gmail.com>
2020-04-25 20:46:41 +0530

    update

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import './collection-item.styles.scss';
+import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className='collection-item'>
+  <div className="collection-item">
     <div
-      className='image'
+      className="image"
       style={{
-        backgroundImage: `url(${imageUrl})`
+        backgroundImage: `url(${imageUrl})`,
       }}
     />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
     </div>
+    <CustomButton inverted>Add to Cart</CustomButton>
   </div>
 );
 
