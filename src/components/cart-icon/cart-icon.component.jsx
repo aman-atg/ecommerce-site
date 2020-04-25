ad9cbc6000a76fbbbf70b79c18af5a8a1cd2eee8
aman-atg <amanansari.a2@gmail.com>
2020-04-25 18:00:50 +0530

    add functionality

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-
+import { connect } from "react-redux";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
-const CartIcon = () => {
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
+
+const CartIcon = ({ toggleCartHidden }) => {
   return (
-    <div className="cart-icon">
+    <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
       <span className="item-count">0</span>
     </div>
   );
 };
 
-export default CartIcon;
+//Actions
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
+
+export default connect(null, mapDispatchToProps)(CartIcon);
