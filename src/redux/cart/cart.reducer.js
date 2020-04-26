c9478b87959f76cd33aff07c7776b354d949e60e
aman-atg <amanansari.a2@gmail.com>
2020-04-26 18:03:29 +0530

    update

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import CartActionTypes from "./cart.types";
+import { addItemToCart } from "./cart.utils";
 
 const { TOGGLE_CART_HIDDEN, ADD_ITEM } = CartActionTypes;
 
@@ -17,7 +18,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case ADD_ITEM:
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload],
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
     default:
       return state;
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -3,11 +3,11 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     cartItem => cartItems.id === cartItemToAdd.id
   );
   if (existingCartItem) {
-    return cartItems.map(cartItem => {
+    return cartItems.map(cartItem =>
       cartItem.id === cartItemToAdd.id
         ? { ...cartItem, quantity: cartItem.quantity + 1 }
-        : cartItem;
-    });
+        : cartItem
+    );
   }
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
