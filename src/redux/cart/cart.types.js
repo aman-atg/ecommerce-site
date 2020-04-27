3128392ecba61bb6016ced9bdbc1e82236ddf239
aman-atg <amanansari.a2@gmail.com>
2020-04-27 14:25:57 +0530

    u

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,8 @@
 const CartActionTypes = {
   TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
   ADD_ITEM: "ADD_ITEM",
+  CLEAR_ITEM: "CLEAR_ITEM",
+  REMOVE_ITEM: "REMOVE_ITEM",
 };
 
 export default CartActionTypes;
