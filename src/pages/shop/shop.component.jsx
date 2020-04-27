668bf153b6d6f44807d459a304307bb60ac26fd5
aman-atg <amanansari.a2@gmail.com>
2020-04-28 01:05:42 +0530

    moved logic out of shopPage for rendering overview

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import SHOP_DATA from './shop.data.js';
-
-import CollectionPreview from '../../components/collection-preview/collection-preview';
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+const ShopPage = () => (
+  <div className="shop-page">
+    <CollectionsOverview />
+  
+  </div>
+);
 
 export default ShopPage;
