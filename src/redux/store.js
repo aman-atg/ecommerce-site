f6d080767dc004f4371d1d340ab61cd34e4c560f
aman-atg <amanansari.a2@gmail.com>
2020-04-27 19:44:34 +0530

    implement redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { applyMiddleware, createStore } from "redux";
+import { persistStore } from "redux-persist";
+
 import { logger } from "redux-logger";
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
