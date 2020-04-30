c774a7408e2dd6a6409f582b43b33a0863cd88c5
aman-atg <amanansari.a2@gmail.com>
2020-04-30 20:42:35 +0530

    using logger only in development env

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import { persistStore } from "redux-persist";
 import { logger } from "redux-logger";
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 const persistor = persistStore(store);
