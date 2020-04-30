837f7353ce4ed7c3166e072b60cf64fff0180deb
aman-atg <amanansari.a2@gmail.com>
2020-04-30 20:37:24 +0530

    update

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -39,7 +39,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="warning">
       *Please use the following test credit card for payments*
       <br />
-      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
     </div>
     <StripeCheckoutButton price={total} />
   </div>
