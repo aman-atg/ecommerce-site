828454d58382cfa776c1749eff84816940ea58d5
aman-atg <amanansari.a2@gmail.com>
2020-05-02 21:32:40 +0530

    u

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -62,12 +62,13 @@ export const convertCollectionsSnapshotToMap = collections => {
   const transformedCollection = collections.docs.map(doc => {
     const { title, items } = doc.data();
     return {
-      routeName: encodeURI(title),
+      routeName: encodeURI(title.toLowerCase()),
       id: doc.id,
       title,
       items,
     };
   });
+
   return transformedCollection.reduce((accumulator, collection) => {
     accumulator[collection.title.toLowerCase()] = collection;
     return accumulator;
