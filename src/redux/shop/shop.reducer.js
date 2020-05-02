f612269d9dd98a3df34b1c243a49d1e7832f4e16
aman-atg <amanansari.a2@gmail.com>
2020-05-02 21:32:09 +0530

    updateCollections

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,16 @@
-import SHOP_DATA from "./shop.data";
+import shopTypes from "./shop.types";
+const { UPDATE_COLLECTIONS } = shopTypes;
 
 const INITIAL_STATE = {
-  collections: SHOP_DATA,
+  collections: null,
 };
-
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case UPDATE_COLLECTIONS:
+      return {
+        ...state,
+        collections: action.payload,
+      };
     default:
       return state;
   }
