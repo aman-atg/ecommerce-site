e3dd2b667e88c8472a96e6da6e757529035b47fc
aman-atg <amanansari.a2@gmail.com>
2020-05-06 03:35:58 +0530

    fetching Actions using thunk

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,7 +1,36 @@
 import shopTypes from "./shop.types";
-const { UPDATE_COLLECTIONS } = shopTypes;
+import {
+  convertCollectionsSnapshotToMap,
+  firestore,
+} from "../../firebase/firebase.utils";
+const {
+  FETCH_COLLECTIONS_START,
+  FETCH_COLLECTIONS_SUCCESS,
+  FETCH_COLLECTIONS_FAILTURE,
+} = shopTypes;
 
-export const updateCollections = collectionsMap => ({
-  type: UPDATE_COLLECTIONS,
-  payload: collectionsMap,
+export const fetchCollectionsStart = () => ({
+  type: FETCH_COLLECTIONS_START,
 });
+export const fetchCollectionsSuccess = collections => ({
+  type: FETCH_COLLECTIONS_SUCCESS,
+  action: collections,
+});
+export const fetchCollectionsFailture = error => ({
+  type: FETCH_COLLECTIONS_FAILTURE,
+  action: error.message,
+});
+
+export const fetchCollectionsStartAsync = () => dispatch => {
+  const collectionRef = firestore.collection("collections");
+  dispatch(fetchCollectionsStart());
+  collectionRef
+    .get()
+    .then(snapshot => {
+      const CollectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      dispatch(fetchCollectionsSuccess(CollectionsMap));
+    })
+    .catch(e => {
+      fetchCollectionsFailture(e);
+    });
+};
