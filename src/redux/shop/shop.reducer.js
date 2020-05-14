3c6c8add9d48732c12ebb573e251ac0325b8f4f2
aman-atg <amanansari.a2@gmail.com>
2020-05-14 22:07:46 +0530

    complete shopReducer

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,37 @@
 import shopTypes from "./shop.types";
-const { UPDATE_COLLECTIONS } = shopTypes;
+const {
+  UPDATE_COLLECTIONS,
+  FETCH_COLLECTIONS_START,
+  FETCH_COLLECTIONS_SUCCESS,
+  FETCH_COLLECTIONS_FAILTURE,
+} = shopTypes;
 
 const INITIAL_STATE = {
   collections: null,
+  isFetching: false,
+  errorMessage: "",
 };
+
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case FETCH_COLLECTIONS_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case FETCH_COLLECTIONS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        collections: action.payload,
+      };
+    case FETCH_COLLECTIONS_FAILTURE:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload,
+      };
+
     case UPDATE_COLLECTIONS:
       return {
         ...state,
