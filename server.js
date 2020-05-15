f5e349453a115d8e5b20dcf8d59a18e2e314cb56
aman-atg <amanansari.a2@gmail.com>
2020-05-15 20:11:43 +0530

    update

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-//express.json() in app
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
