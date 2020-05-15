22c5eab99d12832f017bd034bc27103b8fd44e2e
Aman Ansari <amanansari.a2@gmail.com>
2020-05-15 21:22:09 +0530

    Update stripe-button.component.jsx

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -8,7 +8,7 @@ const StripeCheckoutButton = ({ price }) => {
 
   const onToken = token => {
     axios({
-      url: "payment",
+      url: "/payment",
       method: "post",
       data: {
         amount: priceForStripe,
