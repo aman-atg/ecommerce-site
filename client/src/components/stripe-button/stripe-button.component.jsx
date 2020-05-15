fe8ef4c9d9f9a5277d24e6a7c3199bc7ae046a93
aman-atg <amanansari.a2@gmail.com>
2020-05-16 01:33:16 +0530

    U

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -21,6 +21,8 @@ const StripeCheckoutButton = ({ price }) => {
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+          "Access-Control-Allow-Headers": "Content-Type",
         },
         body: {
           data,
